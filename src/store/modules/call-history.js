453b716f6dadec627efcf6bd2bfda10eaf780aef
Andrii Yakovliev <a.yakovliev@webtrit.com>
2025-03-21 17:08:04 +0200

    fix: simplify check

--- src/store/modules/call-history.js
+++ src/store/modules/call-history.js
@@ -64,8 +64,7 @@ const getters = {
   missedItems(state, getters) {
     let missed = []
     if (getters.items) {
-      missed = getters.items.filter((item) => (item.direction === 'incoming' && item.status === 'missed')
-          || (item.direction === 'incoming' && item.status === 'declined'))
+      missed = getters.items.filter((item) => item.direction === 'incoming' && item.status in ['missed', 'declined'])
     }
     return missed.length > 0 ? missed.slice(0, 10) : []
   },
