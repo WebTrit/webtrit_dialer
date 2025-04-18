bf76bb0b9f55e76964e03786f0e2300ce007ab4c
Roman Yakimenko <r.yakimenko@webtrit.com>
2025-04-18 19:28:02 +0300

    feat: add queue for snackbar notifications (#35)

--- src/store/modules/snackbar.js
+++ src/store/modules/snackbar.js
@@ -3,9 +3,17 @@ const state = () => ({
   visible: false,
   timeout: 7000,
   message: null,
+  queue: [],
+  processing: false,
 })
 
 const mutations = {
+  enqueue(state, message) {
+    state.queue.push(message)
+  },
+  dequeue(state) {
+    state.queue.shift()
+  },
   show(state, message) {
     state.visible = true
     state.message = message
@@ -14,20 +22,42 @@ const mutations = {
     state.visible = false
     state.message = null
   },
+  setProcessing(state, status) {
+    state.processing = status
+  },
 }
 
 const actions = {
   async show({ state, commit }, { message }) {
-    if (state.visible) {
-      commit('hide')
-      await new Promise((r) => setTimeout(r, 200))
+    commit('enqueue', message)
+    if (!state.processing) {
+      const processNextMessage = async () => {
+        if (state.queue.length === 0) {
+          commit('setProcessing', false)
+          return
+        }
+
+        const message = state.queue[0]
+        commit('show', message)
+
+        await new Promise((resolve) => setTimeout(resolve, state.timeout))
+
+        commit('hide')
+        commit('dequeue')
+
+        await new Promise((resolve) => setTimeout(resolve, 200))
+
+        await processNextMessage()
+      }
+
+      commit('setProcessing', true)
+
+      await processNextMessage()
     }
-    commit('show', message)
   },
-  hide({ state, commit }) {
-    if (state.visible) {
-      commit('hide')
-    }
+
+  hide({ commit }) {
+    commit('hide')
   },
 }
 
